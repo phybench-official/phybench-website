a182fd79b451ae82e685c6d37af04778e1e44f3b
wjsoj <wjs@wjsphy.top>
2025-03-23 11:50:42 +0800

    fix: :bug: enable math render with llm format

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  resolve: {
+    alias: {
+      'micromark-extension-math': 'micromark-extension-llm-math'
+    }
+  },
   experimental: {
     turbo: {
       resolveAlias: {
