c42a49bacc0a8aca73958d6a46437d909fd3038b
wjsoj <wjs@wjsphy.top>
2025-03-27 20:57:35 +0800

    feat: :sparkles: add homepage ogl color

--- src/components/main-hero.tsx
+++ src/components/main-hero.tsx
@@ -3,8 +3,15 @@
 import { SplashCursor } from "@/components/ui/splash-cursor"
 import Aurora from "./ui/Aurora";
 import { Hero1 } from "./ui/hero-with-button";
+import { useTheme } from "next-themes";
 
 export function MainHeroSection() {
+  const { resolvedTheme } = useTheme();
+  
+  const colorStops = resolvedTheme === "light" 
+    ? ["#d4c0ff", "#ffc0eb", "#FFC0CB"]
+    : ["#3A29FF", "#FF94B4", "#FF3232"];
+    
   return (
     <>
       <div className="hidden md:block">
@@ -12,14 +19,13 @@ export function MainHeroSection() {
       </div>
       <div className="block md:hidden h-screen w-screen">
         <Aurora 
-          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-          blend={0.5}
+          colorStops={colorStops}
+          blend={1.0}
           amplitude={1.0}
           speed={1.0}
         >
           <Hero1 />
-        </Aurora>
-        
+        </Aurora>        
       </div>
     </>
   );
