330d9dffec35fa1efaf95162ccdb6b44b521bd05
zerohint <11150324+zerohint@user.noreply.gitee.com>
2025-04-11 10:02:28 +0800

    add update-examine-score

---
+++ src/app/admin/update-examine-score/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth";
+import CalculateScorePage from "@/components/admin/update-examine-score";
+import { NotPermitted } from "@/components/ui/not-permitted";
+import { NotAuthorized } from "@/components/ui/not-authorized";
+
+export default async function Page() {
+  const session = await auth();
+  if (!session) return <NotAuthorized />;
+
+  if (session.user.role !== "admin") return <NotPermitted />;
+
+  return (
+    <div className=" h-screen max-h-screen overflow-clip pt-24">
+      <CalculateScorePage />
+    </div>
+  );
+}
